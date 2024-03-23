35bddd047e810d43b7bccc1cd9932cba3246d66d
cauamicael <cauamicael@hotmail.com>
2024-03-23 18:13:09 -0300

    Finalizando Tela do fim do jogo

--- src/App.jsx
+++ src/App.jsx
@@ -138,7 +138,10 @@ function App() {
           score={score}
         />}
 
-      {gameStage === 'end' && <GameOver retry={retry} />}
+      {gameStage === 'end' &&
+        <GameOver
+          retry={retry}
+          score={score}/>}
     </div>
   );
 }
